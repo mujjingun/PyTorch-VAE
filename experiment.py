2373b9bfd47e53dba8361fe18617ba110b82498d
Gun Park <mujjingun@gmail.com>
2020-06-01 19:52:37 +0900

    fix bbvae

--- experiment.py
+++ experiment.py
@@ -40,8 +40,6 @@ class VAEXperiment(pl.LightningModule):
                                               optimizer_idx=optimizer_idx,
                                               batch_idx = batch_idx)
 
-        self.logger.experiment.log({key: val.item() for key, val in train_loss.items()})
-
         return train_loss
 
     def validation_step(self, batch, batch_idx, optimizer_idx = 0):
@@ -69,11 +67,6 @@ class VAEXperiment(pl.LightningModule):
         test_input = test_input.to(self.curr_device)
         test_label = test_label.to(self.curr_device)
         recons = self.model.generate(test_input, labels = test_label)
-        vutils.save_image(recons.data,
-                          f"{self.logger.save_dir}{self.logger.name}/version_{self.logger.version}/"
-                          f"recons_{self.logger.name}_{self.current_epoch}.png",
-                          normalize=True,
-                          nrow=12)
 
         # vutils.save_image(test_input.data,
         #                   f"{self.logger.save_dir}{self.logger.name}/version_{self.logger.version}/"
@@ -85,11 +78,6 @@ class VAEXperiment(pl.LightningModule):
             samples = self.model.sample(144,
                                         self.curr_device,
                                         labels = test_label)
-            vutils.save_image(samples.cpu().data,
-                              f"{self.logger.save_dir}{self.logger.name}/version_{self.logger.version}/"
-                              f"{self.logger.name}_{self.current_epoch}.png",
-                              normalize=True,
-                              nrow=12)
         except:
             pass
 
@@ -141,7 +129,7 @@ class VAEXperiment(pl.LightningModule):
             dataset = CelebA(root = self.params['data_path'],
                              split = "train",
                              transform=transform,
-                             download=True)
+                             download=False)
         else:
             raise ValueError('Undefined dataset type')
 
@@ -159,7 +147,7 @@ class VAEXperiment(pl.LightningModule):
             self.sample_dataloader =  DataLoader(CelebA(root = self.params['data_path'],
                                                         split = "test",
                                                         transform=transform,
-                                                        download=True),
+                                                        download=False),
                                                  batch_size= 144,
                                                  shuffle = False,
                                                  drop_last=True)
--- run_func.py
+++ run_func.py
@@ -6,7 +6,6 @@ from models import *
 from experiment import VAEXperiment
 import torch.backends.cudnn as cudnn
 from pytorch_lightning import Trainer
-from pytorch_lightning.logging import TestTubeLogger
 
 def insert(node, dic):
     for key, value in dic.items():
@@ -17,23 +16,10 @@ def insert(node, dic):
         else:
             node[key] = value
 
-def run_func(filename, **kwargs):
-    with open(filename, 'r') as file:
-        try:
-            config = yaml.safe_load(file)
-        except yaml.YAMLError as exc:
-            print(exc)
+def run_func(config, **kwargs):
+    config = yaml.safe_load(config)
 
     insert(config, kwargs)
-
-    print(config)
-
-    tt_logger = TestTubeLogger(
-        save_dir=config['logging_params']['save_dir'],
-        name=config['logging_params']['name'],
-        debug=False,
-        create_git_tag=False,
-    )
 
     # For reproducibility
     torch.manual_seed(config['logging_params']['manual_seed'])
@@ -45,18 +31,14 @@ def run_func(filename, **kwargs):
     experiment = VAEXperiment(model,
                             config['exp_params'])
 
-    runner = Trainer(default_save_path=f"{tt_logger.save_dir}",
-                    min_nb_epochs=1,
-                    logger=tt_logger,
-                    log_save_interval=100,
+    runner = Trainer(min_nb_epochs=1,
                     train_percent_check=config['exp_params']['fraction'],
                     val_percent_check=1.,
                     num_sanity_val_steps=0,
-                    early_stop_callback = False,
+                    early_stop_callback=False,
+                    weights_summary=None,
                     **config['trainer_params'])
-
-    print(f"======= Training {config['model_params']['name']} =======")
     runner.fit(experiment)
 
-    print("======= Evaluating Trained Model =======")
+    print("validation loss = ", experiment.val_loss.item())
     return experiment.val_loss.item()
